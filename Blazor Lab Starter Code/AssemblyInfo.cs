5a3b7e946f9714491519118ef59ff885038a12b0
Newellp1 <newellp1@etsu.edu>
2024-11-19 19:56:45 -0500

    Add project files.

---
+++ Blazor Lab Starter Code/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.CompilerServices;
+
+// Grant the test project access to internal members of this project
+[assembly: InternalsVisibleTo("BlazorLabStarterCodeTests")]
+
+namespace Blazor_Lab_Starter_Code
+{
+    // Assembly-level attributes like InternalsVisibleTo are declared here.
+    internal class AssemblyInfo
+    {
+    }
+}
---
+++ BlazorLabStarterCodeTests/ProgramTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Blazor_Lab_Starter_Code;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+// This is a test change to trigger the CI workflow
+
+namespace BlazorLabStarterCodeTests
+{
+    [TestClass]
+    public class ProgramTests
+    {
+        [TestInitialize]
+        public void TestSetup()
+        {
+            Blazor_Lab_Starter_Code.Program.books = new List<Book>();
+            Blazor_Lab_Starter_Code.Program.users = new List<User>();
+            Blazor_Lab_Starter_Code.Program.borrowedBooks = new Dictionary<User, List<Book>>();
+        }
+
+        [TestMethod]
+        public void AddUser_ValidInput_UserAdded()
+        {
+            var input = new StringReader("User1\nuser1@example.com\n");
+            Console.SetIn(input);
+            Blazor_Lab_Starter_Code.Program.AddUser();
+
+            Assert.AreEqual(1, Blazor_Lab_Starter_Code.Program.users.Count);
+            Assert.AreEqual("User1", Blazor_Lab_Starter_Code.Program.users[0].Name);
+        }
+
+        [TestMethod]
+        public void BorrowBook_ValidBorrow_Success()
+        {
+            Blazor_Lab_Starter_Code.Program.books.Add(new Book { Id = 1, Title = "Sample Book", Author = "Author A" });
+            Blazor_Lab_Starter_Code.Program.users.Add(new User { Id = 1, Name = "User A" });
+
+            var input = new StringReader("1\n1");
+            Console.SetIn(input);
+
+            Blazor_Lab_Starter_Code.Program.BorrowBook();
+
+            Assert.AreEqual(0, Blazor_Lab_Starter_Code.Program.books.Count);
+            Assert.AreEqual(1, Blazor_Lab_Starter_Code.Program.borrowedBooks[Blazor_Lab_Starter_Code.Program.users[0]].Count);
+        }
+    }
+}
